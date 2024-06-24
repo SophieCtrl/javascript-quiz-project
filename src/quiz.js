789b8971f228a21b627ddb8373d3146de8c60f21
Sophie <sallysophie@hotmail.de>
2024-06-24 17:16:49 +0200

    1st day

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,66 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    // this.randomQuestion = this.choices[Math.floor(Math.random * this.choices.length)];
+    for (let i = this.questions.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 6. hasEnded()
+  checkAnswers(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer)
+      this.correctAnswers += 1;
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) return false;
+    return true;
+  }
+
+  // 2. getQuestion()
+
+  // 3. moveToNextQuestion()
+
+  // 4. shuffleQuestions()
+
+  // 5. checkAnswer(answer)
+
+  // 6. hasEnded()
+
+  // day2
+  filterQuestionsByDifficulty(difficulty) {
+    //if (isNaN(this.difficulty)) return;
+    if (isNaN(difficulty) || difficulty < 1 || difficulty > 3) {
+      return [];
+    }
+
+    // Filter questions based on difficulty
+    this.questions = this.questions.filter(
+      (element) => element.difficulty === difficulty
+    );
+
+    return this.questions;
+  }
 }
